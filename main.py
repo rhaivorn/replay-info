528ff73e1ed722ac0e3829fec88ce4b009e131ab
rhaivorn <eswirddunkel101@gmail.com>
2025-04-17 09:07:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import replay_viewer
+from multiprocessing import freeze_support
 
+def main():
+    app = replay_viewer.ReplayViewer(False)
+    app.MainLoop()
 
-app = replay_viewer.ReplayViewer(False)
-app.MainLoop()
+if __name__ == "__main__":
+    freeze_support()
+    main()
