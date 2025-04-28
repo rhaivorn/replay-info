cd924c73685624863ac85a44a1c7340b8f35c6d3
rhaivorn <eswirddunkel101@gmail.com>
2025-04-28 06:08:57 +0000

    Add files via upload

---
+++ version_config.py
@@ -0,0 +1,30 @@
+version_config = { 
+    "default": {
+        "colors": {
+            0: ["Gold", "#CC9900"], 
+            1: ["Red", "#C80000"], 
+            2: ["Blue", "#0066CC"], 
+            3: ["Green", "#008000"], 
+            4: ["Orange", "#FF6600"], 
+            5: ["Cyan", "#0096b4"], 
+            6: ["Purple", "#800080"], 
+            7: ["Pink", "#C83296"]
+        },
+        "factions": {
+            0: ["USA", "usa"], 
+            1: ["China", "china"], 
+            2: ["GLA", "gla"], 
+            3: ["USA Superweapon", "sw"], 
+            4: ["USA Laser", "laz"], 
+            5: ["USA Airforce", "air"], 
+            6: ["China Tank", "tank"], 
+            7: ["China Infantry", "inf"], 
+            8: ["China Nuke", "nuke"], 
+            9: ["GLA Toxin", "tox"], 
+            10: ["GLA Demolition", "demo"], 
+            11: ["GLA Stealth", "stlth"]
+        }
+    },
+
+    # Add colors and factions here for different versions using the version string stored in the rep file as the key.
+}
