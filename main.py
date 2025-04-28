53f36d7e13fddb43f9ab71ffc4d503315f29fbb6
rhaivorn <eswirddunkel101@gmail.com>
2025-04-28 06:01:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import replay_viewer
-from multiprocessing import freeze_support
+from multiprocessing import freeze_support, set_start_method
 
 def main():
     app = replay_viewer.ReplayViewer(False)
@@ -7,4 +7,5 @@ def main():
 
 if __name__ == "__main__":
     freeze_support()
+    set_start_method("spawn", force=True)
     main()
